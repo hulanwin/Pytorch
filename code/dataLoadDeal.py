962f4a54374b0801173e665954e50ad13f3b80bb
Hou Biao <14747367759@163.com>
2020-08-05 15:41:49 +0800

    Add files via upload

---
+++ code/dataLoadDeal.py
@@ -0,0 +1,258 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd  # 用于更容易地进行csv解析
+from skimage import io, transform  # 用于图像的IO和变换
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# 忽略警告
+import warnings
+
+warnings.filterwarnings("ignore")
+
+plt.ion()  # interactive mode
+
+# 读取数据集
+landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
+
+n = 65
+img_name = landmarks_frame.iloc[n, 0]
+landmarks = landmarks_frame.iloc[n, 1:].values
+landmarks = landmarks.astype('float').reshape(-1, 2)
+
+print('Image name: {}'.format(img_name))
+print('Landmarks shape: {}'.format(landmarks.shape))
+print('First 4 Landmarks: {}'.format(landmarks[:4]))
+
+
+# 展示一张图片和它对应的标注点
+def show_landmarks(image, landmarks):
+    """显示带有地标的图片"""
+    plt.imshow(image)
+    plt.scatter(landmarks[:, 0], landmarks[:, 1], s=10, marker='.', c='r')
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+plt.figure()
+show_landmarks(io.imread(os.path.join('data/faces/', img_name)),
+               landmarks)
+plt.show()
+
+
+# 建立数据集类
+class FaceLandmarksDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+          Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)  # 初始化时就把csv文件读入
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):  # 返回数据集总大小
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+# 数据可视化
+face_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                    root_dir='data/faces/')
+
+fig = plt.figure()
+
+for i in range(len(face_dataset)):
+    sample = face_dataset[i]
+
+    print(i, sample['image'].shape, sample['landmarks'].shape)
+
+    ax = plt.subplot(1, 4, i + 1)
+    plt.tight_layout()
+    ax.set_title('Sample #{}'.format(i))
+    ax.axis('off')
+    show_landmarks(**sample)
+
+    if i == 3:
+        plt.show()
+        break
+
+
+#数据变换
+class Rescale(object):
+    """将样本中的图像重新缩放到给定大小。.
+
+    Args:
+        output_size（tuple或int）：所需的输出大小。 如果是元组，则输出为
+         与output_size匹配。 如果是int，则匹配较小的图像边缘到output_size保持纵横比相同。
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, landmarks = sample['image'], sample['landmarks']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        # h and w are swapped for landmarks because for images,
+        # x and y axes are axis 1 and 0 respectively
+        landmarks = landmarks * [new_w / w, new_h / h]
+
+        return {'image': img, 'landmarks': landmarks}
+
+
+class RandomCrop(object):
+    """随机裁剪样本中的图像.
+
+    Args:
+       output_size（tuple或int）：所需的输出大小。 如果是int，方形裁剪是。
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, landmarks = sample['image'], sample['landmarks']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        landmarks = landmarks - [left, top]
+
+        return {'image': image, 'landmarks': landmarks}
+
+
+class ToTensor(object):
+    """将样本中的ndarrays转换为Tensors."""
+
+    def __call__(self, sample):
+        image, landmarks = sample['image'], sample['landmarks']
+
+        # 交换颜色轴因为
+        # numpy包的图片是: H * W * C
+        # torch包的图片是: C * H * W
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'landmarks': torch.from_numpy(landmarks)}
+
+
+#组合转换
+scale = Rescale(256)
+crop = RandomCrop(128)
+composed = transforms.Compose([Rescale(256),
+                               RandomCrop(224)])
+
+# 在样本上应用上述的每个变换。
+fig = plt.figure()
+sample = face_dataset[65]
+for i, tsfrm in enumerate([scale, crop, composed]):
+    transformed_sample = tsfrm(sample)
+
+    ax = plt.subplot(1, 3, i + 1)
+    plt.tight_layout()
+    ax.set_title(type(tsfrm).__name__)
+    show_landmarks(**transformed_sample)
+
+plt.show()
+
+
+#迭代数据集
+transformed_dataset = FaceLandmarksDataset(csv_file='data/faces/face_landmarks.csv',
+                                           root_dir='data/faces/',
+                                           transform=transforms.Compose([
+                                               Rescale(256),
+                                               RandomCrop(224),
+                                               ToTensor()
+                                           ]))
+
+for i in range(len(transformed_dataset)):
+    sample = transformed_dataset[i]
+
+    print(i, sample['image'].size(), sample['landmarks'].size())
+
+    if i == 3:
+        break
+
+
+
+dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=0)
+
+
+# 辅助功能：显示批次
+def show_landmarks_batch(sample_batched):
+    """Show image with landmarks for a batch of samples."""
+    images_batch, landmarks_batch = \
+            sample_batched['image'], sample_batched['landmarks']
+    batch_size = len(images_batch)
+    im_size = images_batch.size(2)
+    grid_border_size = 2
+
+    grid = utils.make_grid(images_batch)
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+
+    for i in range(batch_size):
+        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size + (i + 1) * grid_border_size,
+                    landmarks_batch[i, :, 1].numpy() + grid_border_size,
+                    s=10, marker='.', c='r')
+
+        plt.title('Batch from dataloader')
+
+for i_batch, sample_batched in enumerate(dataloader):
+    print(i_batch, sample_batched['image'].size(),
+          sample_batched['landmarks'].size())
+
+    # 观察第4批次并停止。
+    if i_batch == 3:
+        plt.figure()
+        show_landmarks_batch(sample_batched)
+        plt.axis('off')
+        plt.ioff()
+        plt.show()
+        break
---
+++ code/pytorchtest.py
@@ -0,0 +1,266 @@
+from __future__ import print_function
+import torch
+
+#不初始化
+#x=torch.empty(5,3)
+#print(x)
+
+#随机初始化
+#x=torch.rand(5,3)
+#print(x)
+
+#Construct a matrix filled zeros and of dtype long:
+##print(x)
+
+#x = torch.tensor([5.5, 3])
+#print(x)
+
+
+#x =x.new_ones(5, 3, dtype=torch.float64)  # 返回的tensor默认具有相同的torch.dtype和torch.device
+#print(x)
+
+#x = torch.randn_like(x, dtype=torch.float)  # 指定新的数据类型
+#print(x)
+
+#print(x.size())
+"""
+x=torch.rand(5,3)
+print(x)
+y=torch.rand(5,3)
+print(y)
+#print(x+y)
+print(torch.add(x,y))
+
+result=torch.rand(5,3)
+torch.add(x,y,out=result)
+print(result)
+
+y.add_(x)
+print(y)
+
+x=torch.randn(4,4)
+y=x.view(16)
+z=x.view(-1,8)
+print(x.size(),y.size(),z.size())
+
+h=torch.randn(1)
+print(h)
+print(h.item())
+
+print(x[:,1])
+"""
+
+x=torch.ones(2,2,requires_grad=True)
+print(x)
+
+y=x+2
+print(y)
+print(y.grad_fn)
+
+a = torch.randn(2, 2)
+a = ((a * 3) / (a - 1))
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b = (a * a).sum()
+print(b.grad_fn)
+
+z=y*y*3
+out=z.mean()
+print(z,out)
+
+out.backward()
+print(x.grad)
+
+
+z=y*y*3
+out=z.mean()
+print(z,out)
+
+# -*- coding: utf-8 -*-
+"""
+Autograd: Automatic Differentiation
+===================================
+
+Central to all neural networks in PyTorch is the ``autograd`` package.
+Let’s first briefly visit this, and we will then go to training our
+first neural network.
+
+
+The ``autograd`` package provides automatic differentiation for all operations
+on Tensors. It is a define-by-run framework, which means that your backprop is
+defined by how your code is run, and that every single iteration can be
+different.
+
+Let us see this in more simple terms with some examples.
+
+Tensor
+--------
+
+``torch.Tensor`` is the central class of the package. If you set its attribute
+``.requires_grad`` as ``True``, it starts to track all operations on it. When
+you finish your computation you can call ``.backward()`` and have all the
+gradients computed automatically. The gradient for this tensor will be
+accumulated into ``.grad`` attribute.
+
+To stop a tensor from tracking history, you can call ``.detach()`` to detach
+it from the computation history, and to prevent future computation from being
+tracked.
+
+To prevent tracking history (and using memory), you can also wrap the code block
+in ``with torch.no_grad():``. This can be particularly helpful when evaluating a
+model because the model may have trainable parameters with `requires_grad=True`,
+but for which we don't need the gradients.
+
+There’s one more class which is very important for autograd
+implementation - a ``Function``.
+
+``Tensor`` and ``Function`` are interconnected and build up an acyclic
+graph, that encodes a complete history of computation. Each tensor has
+a ``.grad_fn`` attribute that references a ``Function`` that has created
+the ``Tensor`` (except for Tensors created by the user - their
+``grad_fn is None``).
+
+If you want to compute the derivatives, you can call ``.backward()`` on
+a ``Tensor``. If ``Tensor`` is a scalar (i.e. it holds a one element
+data), you don’t need to specify any arguments to ``backward()``,
+however if it has more elements, you need to specify a ``gradient``
+argument that is a tensor of matching shape.
+"""
+
+import torch
+
+###############################################################
+# Create a tensor and set requires_grad=True to track computation with it
+x = torch.ones(2, 2, requires_grad=True)
+print(x)
+
+###############################################################
+# Do an operation of tensor:
+y = x + 2
+print(y)
+
+###############################################################
+# ``y`` was created as a result of an operation, so it has a ``grad_fn``.
+print(y.grad_fn)
+
+###############################################################
+# Do more operations on y
+z = y * y * 3
+out = z.mean()
+
+print(z, out)
+
+################################################################
+# ``.requires_grad_( ... )`` changes an existing Tensor's ``requires_grad``
+# flag in-place. The input flag defaults to ``False`` if not given.
+a = torch.randn(2, 2)
+a = ((a * 3) / (a - 1))
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b = (a * a).sum()
+print(b.grad_fn)
+
+###############################################################
+# Gradients
+# ---------
+# Let's backprop now
+# Because ``out`` contains a single scalar, ``out.backward()`` is
+# equivalent to ``out.backward(torch.tensor(1.))``.
+
+out.backward()
+
+###############################################################
+# print gradients d(out)/dx
+#
+
+print(x.grad)
+
+###############################################################
+# You should have got a matrix of ``4.5``. Let’s call the ``out``
+# *Tensor* “:math:`o`”.
+# We have that :math:`o = \frac{1}{4}\sum_i z_i`,
+# :math:`z_i = 3(x_i+2)^2` and :math:`z_i\bigr\rvert_{x_i=1} = 27`.
+# Therefore,
+# :math:`\frac{\partial o}{\partial x_i} = \frac{3}{2}(x_i+2)`, hence
+# :math:`\frac{\partial o}{\partial x_i}\bigr\rvert_{x_i=1} = \frac{9}{2} = 4.5`.
+
+###############################################################
+# Mathematically, if you have a vector valued function :math:`\vec{y}=f(\vec{x})`,
+# then the gradient of :math:`\vec{y}` with respect to :math:`\vec{x}`
+# is a Jacobian matrix:
+#
+# .. math::
+#   J=\left(\begin{array}{ccc}
+#    \frac{\partial y_{1}}{\partial x_{1}} & \cdots & \frac{\partial y_{m}}{\partial x_{1}}\\
+#    \vdots & \ddots & \vdots\\
+#    \frac{\partial y_{1}}{\partial x_{n}} & \cdots & \frac{\partial y_{m}}{\partial x_{n}}
+#    \end{array}\right)
+#
+# Generally speaking, ``torch.autograd`` is an engine for computing
+# Jacobian-vector product. That is, given any vector
+# :math:`v=\left(\begin{array}{cccc} v_{1} & v_{2} & \cdots & v_{m}\end{array}\right)^{T}`,
+# compute the product :math:`J\cdot v`. If :math:`v` happens to be
+# the gradient of a scalar function :math:`l=g\left(\vec{y}\right)`,
+# that is,
+# :math:`v=\left(\begin{array}{ccc}\frac{\partial l}{\partial y_{1}} & \cdots & \frac{\partial l}{\partial y_{m}}\end{array}\right)^{T}`,
+# then by the chain rule, the Jacobian-vector product would be the
+# gradient of :math:`l` with respect to :math:`\vec{x}`:
+#
+# .. math::
+#   J\cdot v=\left(\begin{array}{ccc}
+#    \frac{\partial y_{1}}{\partial x_{1}} & \cdots & \frac{\partial y_{m}}{\partial x_{1}}\\
+#    \vdots & \ddots & \vdots\\
+#    \frac{\partial y_{1}}{\partial x_{n}} & \cdots & \frac{\partial y_{m}}{\partial x_{n}}
+#    \end{array}\right)\left(\begin{array}{c}
+#    \frac{\partial l}{\partial y_{1}}\\
+#    \vdots\\
+#    \frac{\partial l}{\partial y_{m}}
+#    \end{array}\right)=\left(\begin{array}{c}
+#    \frac{\partial l}{\partial x_{1}}\\
+#    \vdots\\
+#    \frac{\partial l}{\partial x_{n}}
+#    \end{array}\right)
+#
+# This characteristic of Jacobian-vector product makes it very
+# convenient to feed external gradients into a model that has
+# non-scalar output.
+
+###############################################################
+# Now let's take a look at an example of Jacobian-vector product:
+
+x = torch.randn(3, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+
+print(y)
+
+###############################################################
+# Now in this case ``y`` is no longer a scalar. ``torch.autograd``
+# could not compute the full Jacobian directly, but if we just
+# want the Jacobian-vector product, simply pass the vector to
+# ``backward`` as argument:
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+
+print(x.grad)
+
+###############################################################
+# You can also stop autograd from tracking history on Tensors
+# with ``.requires_grad=True`` by wrapping the code block in
+# ``with torch.no_grad()``:
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+	print((x ** 2).requires_grad)
+
+###############################################################
+# **Read Later:**
+#
+# Documentation of ``autograd`` and ``Function`` is at
+# https://pytorch.org/docs/autograd
+
